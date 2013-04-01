fd79b21604528d6aa403dfba14a76ba157af2dc2
mvogelsong1 <mvogelsong1@gmail.com>
2013-03-31 22:43:37 -0400

    Changed Files to Folders for Arduino

---
+++ Tutorial5/Tutorial5.ino
@@ -0,0 +1,40 @@
+/*
+Lab Group 7
+Tutorial 5
+*/
+
+int switchPin = 8;
+int ledPin = 11;
+boolean lastButton = LOW;
+int ledLevel = 0;
+boolean currentButton = LOW;
+
+void setup()
+{
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(switchPin);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+  return current;
+}
+
+void loop()
+{
+  currentButton = debounce(lastButton);
+  if (currentButton == HIGH && lastButton == LOW)
+  {
+    ledLevel = ledLevel + 51;
+  }
+  lastButton = currentButton;
+  if (ledLevel > 255) ledLevel = 0;
+  analogWrite(ledPin, ledLevel);
+}
+
